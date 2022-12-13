f532926b7ce41ca3fad40ac1786d41ce929e0f8a
Digital-Grinnell <digital@grinnell.edu>
2022-12-13 16:39:07 -0600

    More script improvements

--- transform-mods-csv-to-ready-for-CB.py
+++ transform-mods-csv-to-ready-for-CB.py
@@ -6,31 +6,25 @@
 ## and transform that data into a new ready-for-CB datetime-named tab of the same Google Sheet, but using the column 
 ## heading/structure of the CollectionBuilder demo `Sheet1` tab.
 
-# import glob
-# import pathlib
-# import sys
-# from queue import Empty
-# from typing import Dict
-
 import os
 import csv
 import inspect
-import tempfile
+import re
 import gspread as gs
 from datetime import datetime
 
 # Populate the "transform" dict.  THIS IS CRITICAL!  
 # This dict maps MODS.csv headings either to a CB CSV heading, or to a function.
 transform = {
-  "PID": "objectid",
+  "PID": { "pid": "objectid" },
   "WORKSPACE": None,
   "Import_Index": { "tbd": None },
   "PARENT": "parentid",
-  "CMODEL": { "simple_map": "display_template" },
+  "CMODEL": { "cmodel_map": "display_template" },
   "SEQUENCE": { "tbd": None },
-  "OBJ": { "filename": "object_location" },
+  "OBJ": { "obj": "object_location" },
   "TRANSCRIPT": { "filename": "transcript" },
-  "THUMBNAIL": { "filename": "image_thumb" },
+  "THUMBNAIL": { "thumbnail": "image_thumb" },
   "Title": "title",
   "Alternative_Titles": { "tbd": None },
   "Personal_Names~Roles": { "name_with_attribute": "creator" },
@@ -84,6 +78,38 @@ required_by_CB = [
   "format"
 ]
 
+# Delcaring some "global" variables for use between functions 
+thumbnail_image = ""
+objectID = ""
+
+# display_template:
+#
+#     A template type used for the Item page and used in logic to choose representations in other pages.
+#     If blank the object will default to a generic item page.
+#     Supported values in display_template match files found in “_layouts”.
+#     Default supported options: image, pdf, video, audio, record, item.
+#         image: Displays image_small if available, with fall back to object_location. Adds gallery view to open images full screen using object_location, with fall back to image_small.
+#         pdf: Displays image_small if available, with fall back to image_thumb, or a pdf icon.
+#         video: Displays a video embedded on the page with default support for video files (using <video> element with object_location as src), YouTube (from link in object_location), or Vimeo videos (from link in object_location).
+#         audio: Uses <audio> element to embed audio file from object_location as src.
+#         record: metadata only record.
+#         item: generic fallback item page, displays image or icon depending on “image_thumb”
+#     See “docs/item-pages.md” in your CollectionBuilder-CSV project repository for more details.
+
+CModels = {
+  "islandora:binaryObjectCModel": "item",
+  "islandora:bookCModel": "item",
+  "islandora:sp_pdf": "pdf",
+  "islandora:compoundCModel": "record",
+  "islandora:sp-audioCModel": "audio",
+  "islandora:oralhistoriesCModel": "audio",
+  "islandora:sp_large_image_cmodel": "image",
+  "islandora:sp_web_archive": "item",
+  "islandora:sp_videoCModel": "video",
+  "islandora:sp_basic_image": "image",
+  "islandora:pageCModel": "item"
+}
+
 # tbd: no transform defined at this time
 def tbd( value, from_column, to ):
   func = inspect.currentframe().f_code.co_name
@@ -104,15 +130,62 @@ def name_with_attribute( value, from_column, to ):
     print("Function '{}' for column '{}' called with argument '{}' but I don't know what to do!".format(func, from_column, value))
   return False
 
-# filename: a filepath or URL to be translated as-is to a new column
+# pid: special handling for the object's PID
+def pid( value, from_column, to ):
+  global objectID
+  func = inspect.currentframe().f_code.co_name
+  if to is None:
+    print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
+    return False
+  else:
+    s = sanitized(value, from_column, to)
+    objectID = s
+    return s
+
+# sanitized: a string to be translated as-is to a new column AFTER sanitization
+def sanitized( value, from_column, to ):
+  func = inspect.currentframe().f_code.co_name
+  if to is None:
+    print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
+    return False
+  else:
+    sanitized = re.sub(r'[^\w\d-]', '_', value)
+    return sanitized
+
+# obj: the object URL and a special transform 
+def obj( value, from_column, to ):
+  global thumbnail_image
+  func = inspect.currentframe().f_code.co_name
+  if to is None:
+    print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
+    return False
+  else:
+    pid = value
+    thumbnail_image = value + "/datastream/TN/view"
+    return value + "/datastream/OBJ/view"
+
+# thumbnail: a TN datastream to populate the 'image_thumb' AND 'image_small' fields
+def thumbnail( value, from_column, to ):
+  global thumbnail_image
+  global transformed
+  func = inspect.currentframe().f_code.co_name
+  if to is None:
+    print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
+    return False
+  elif value in thumbnail_image:
+    transformed['image_small'] = thumbnail_image
+    return thumbnail_image
+  else:
+    return False  
+
+# filename: a filepath or URL to be transformed to a new column
 def filename( value, from_column, to ):
   func = inspect.currentframe().f_code.co_name
   if to is None:
     print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
     return False
   else:
-    print("Function '{}' for column '{}' called with argument '{}' but I don't know what to do!".format(func, from_column, value))
-  return False
+    return value 
 
 # simple_list: a simple list to translate into a single-value column
 def simple_list( value, from_column, to ):
@@ -124,46 +197,22 @@ def simple_list( value, from_column, to ):
     print("Function '{}' for column '{}' called with argument '{}' but I don't know what to do!".format(func, from_column, value))
   return False
 
-# simple_map: a single value from controlled vocab to translate into a single-value column
-def simple_map( value, from_column, to ):
+# cmodel_map: a single value from controlled vocab to translate into a single-value column
+def cmodel_map( value, from_column, to ):
   func = inspect.currentframe().f_code.co_name
   if to is None:
     print("Transform function '{}' for column '{}' maps to None, skip it!".format(func, from_column))
     return False
   else:
-    print("Function '{}' for column '{}' called with argument '{}' but I don't know what to do!".format(func, from_column, value))
-  return False
+    return CModels[value]
 
-# Lifted from https://stackoverflow.com/a/54231563
-#  csv_file - path to csv file to upload
-#  sheet - a gspread.Spreadsheet object
-#  cell - string giving starting cell, optionally including sheet/tab name
-#    example: 'A1', 'MySheet!C3', etc.
-def paste_csv(csv_file, sheet, cell):
-  if '!' in cell:
-    (tab_name, cell) = cell.split('!')
-    wks = sheet.worksheet(tab_name)
-  else:
-    wks = sheet.sheet1
-  (first_row, first_column) = gs.utils.a1_to_rowcol(cell)
-
-  with open(csv_file, 'r') as f:
-    csv_contents = f.read()
-  body = {
-    'requests': [{
-      'pasteData': {
-        "coordinate": {
-          "sheetId": wks.id,
-          "rowIndex": first_row-1,
-          "columnIndex": first_column-1,
-        },
-        "data": csv_contents,
-        "type": 'PASTE_NORMAL',
-        "delimiter": ',',
-      }
-    }]
-  }
-  return sheet.batch_update(body)
+# Lifted from https://stackoverflow.com/questions/57264871/python-gspread-import-csv-to-specific-work-sheet
+def paste_csv( csvFile, sh, sheetName ):
+  sh.values_update(
+    sheetName,
+    params={'valueInputOption': 'USER_ENTERED'},
+    body={'values': list(csv.reader(open(csvFile)))}
+  )
 
 ######################################################################
 
@@ -244,6 +293,9 @@ if __name__ == '__main__':
     except Exception as e:
       print(e)  
 
+    nCols = len(new_headings)
+    nRows = 1  
+
     # Loop on each record of data_records, and on each 'column' of the record, fetch the corresponding transform and apply it
     for record in data_records:
       transformed = dict.fromkeys(new_headings)
@@ -256,7 +308,7 @@ if __name__ == '__main__':
           func = globals()[key]
           r = func(record[column], column, t[key])
           if r:
-            transformed[t[0]] = r
+            transformed[t[key]] = r
         elif t is None:                     # NO transform, skip this column 
           pass
         else:  
@@ -265,26 +317,30 @@ if __name__ == '__main__':
       # Write the transformed record
       try:
         csvwriter.writerow(transformed.values())
+        nRows += 1
       except Exception as e:
         print(e)  
   
+    # Done writing to `transformed.csv`, close it to ensure the buffers are flushed
+    csvfile.close()
+
     # Write the temporary transformed .csv to a new tab in our Google Sheet
     # Make a datestamp to name the new worksheet
     new_tab = datetime.now().strftime("%Y-%b-%d-%I:%M%p")
   
     # Create the new/empty worksheet
     try:
-      worksheet = sh.add_worksheet(title=new_tab, rows=1, cols=1)
+      worksheet = sh.add_worksheet(title=new_tab, rows=nRows, cols=nCols)
     except Exception as e:
       print(e)  
 
     # Call our function to write the new Google Sheet worksheet
     try:
-      paste_csv('transformed.csv', sh, new_tab + "!A1")
+      paste_csv('transformed.csv', sh, new_tab)
     except Exception as e:
       print(e)
 
     # Delete the temp file
-    os.remove('transformed.csv')
+    # os.remove('transformed.csv')    # Keep the file during script development and testing
 
   exit( )
